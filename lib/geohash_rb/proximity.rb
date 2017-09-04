b9bb81770563270646b70dc005410103156bc855
Luiz Paulo Godoy <luizpaulogodoy@gmail.com>
2017-09-04 13:56:58 -0300

    removing blank lines and updating version

--- lib/geohash_rb/proximity.rb
+++ lib/geohash_rb/proximity.rb
@@ -11,7 +11,6 @@ module GeohashRb::Proximity
     module_function :in_circle_check
     
     def get_centroid(latitude, longitude, height, width)
-      
       y_cen = latitude + (height / 2)
       x_cen = longitude + (width / 2)
       
@@ -35,56 +34,52 @@ module GeohashRb::Proximity
     module_function :convert_to_latlon
 
     def create_geohash(latitude, longitude, radius, precision, georaptor_flag=false, minlevel=1, maxlevel=12)
-      
-          x = 0.0
-          y = 0.0
-      
-          points = []
-          geohashes = []
-      
-          grid_width = [5009400.0, 1252300.0, 156500.0, 39100.0, 4900.0, 1200.0, 152.9, 38.2, 4.8, 1.2, 0.149, 0.0370]
-          grid_height = [4992600.0, 624100.0, 156000.0, 19500.0, 4900.0, 609.4, 152.4, 19.0, 4.8, 0.595, 0.149, 0.0199]
-      
-          height = (grid_height[precision - 1]) / 2
-          width = (grid_width[precision - 1]) / 2
-      
-          lat_moves = (radius / height).ceil #4
-          lon_moves = (radius / width).ceil #2
-      
-          for i in 0...lat_moves
-      
-            temp_lat = y + height * i
-    
-            for j in 0...lon_moves
-    
-              temp_lon = x + width * j
+      x = 0.0
+      y = 0.0
   
-              if in_circle_check(temp_lat, temp_lon, y, x, radius)
+      points = []
+      geohashes = []
   
-                  x_cen, y_cen = get_centroid(temp_lat, temp_lon, height, width)
+      grid_width = [5009400.0, 1252300.0, 156500.0, 39100.0, 4900.0, 1200.0, 152.9, 38.2, 4.8, 1.2, 0.149, 0.0370]
+      grid_height = [4992600.0, 624100.0, 156000.0, 19500.0, 4900.0, 609.4, 152.4, 19.0, 4.8, 0.595, 0.149, 0.0199]
   
-                  lat, lon = convert_to_latlon(y_cen, x_cen, latitude, longitude)
-                  points += [[lat, lon]]
-                  lat, lon = convert_to_latlon(-y_cen, x_cen, latitude, longitude)
-                  points += [[lat, lon]]
-                  lat, lon = convert_to_latlon(y_cen, -x_cen, latitude, longitude)
-                  points += [[lat, lon]]
-                  lat, lon = convert_to_latlon(-y_cen, -x_cen, latitude, longitude)
-                  points += [[lat, lon]]
-              end
-            end
-          end
+      height = (grid_height[precision - 1]) / 2
+      width = (grid_width[precision - 1]) / 2
+  
+      lat_moves = (radius / height).ceil #4
+      lon_moves = (radius / width).ceil #2
+  
+      for i in 0...lat_moves
+        temp_lat = y + height * i
 
-          for point in points
-            geohashes += [GeohashRb.encode(point[0], point[1], precision)]
+        for j in 0...lon_moves
+          temp_lon = x + width * j
+
+          if in_circle_check(temp_lat, temp_lon, y, x, radius)
+            x_cen, y_cen = get_centroid(temp_lat, temp_lon, height, width)
+
+            lat, lon = convert_to_latlon(y_cen, x_cen, latitude, longitude)
+            points += [[lat, lon]]
+            lat, lon = convert_to_latlon(-y_cen, x_cen, latitude, longitude)
+            points += [[lat, lon]]
+            lat, lon = convert_to_latlon(y_cen, -x_cen, latitude, longitude)
+            points += [[lat, lon]]
+            lat, lon = convert_to_latlon(-y_cen, -x_cen, latitude, longitude)
+            points += [[lat, lon]]
           end
-      
-          if georaptor_flag
-            georaptor_out = compress(::Set.new(geohashes), minlevel, maxlevel)
-            return georaptor_out.to_a.join(',')
-          else
-            return ::Set.new(geohashes).to_a.join(',')
-          end
+        end
+      end
+
+      for point in points
+        geohashes += [GeohashRb.encode(point[0], point[1], precision)]
+      end
+  
+      if georaptor_flag
+        georaptor_out = compress(::Set.new(geohashes), minlevel, maxlevel)
+        return georaptor_out.to_a.join(',')
+      else
+        return ::Set.new(geohashes).to_a.join(',')
+      end
     end
     module_function :create_geohash
 
@@ -92,7 +87,6 @@ module GeohashRb::Proximity
   def get_combinations(str)
     base32 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'm',
               'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    # [str + '{0}'.format(i) for i in base32]
     array = []
     for i in base32
       str + i
@@ -101,7 +95,7 @@ module GeohashRb::Proximity
   end
   module_function :get_combinations
 
-    # Recursive optimization of the geohash set
+  # Recursive optimization of the geohash set
   def compress(geohashes, minlevel, maxlevel)
     deletegh = Set.new
     final_geohashes = Set.new
@@ -115,58 +109,54 @@ module GeohashRb::Proximity
     end  
 
     while flag do
-
       final_geohashes.clear()
       deletegh.clear()
 
       for geohash in geohashes
-
         geohash_length = geohash.size
 
         # Compress only if geohash length is greater than the min level
-          if geohash_length >= minlevel
-            # Get geohash to generate combinations for
-            part = geohash[-1]
+        if geohash_length >= minlevel
+          # Get geohash to generate combinations for
+          part = geohash[-1]
 
-            # Proceed only if not already processed
-            if not deletegh.include?(part) and not deletegh.include?(geohash)
+          # Proceed only if not already processed
+          if not deletegh.include?(part) and not deletegh.include?(geohash)
+            # Generate combinations
+            combinations = Set.new(get_combinations(part))
 
-              # Generate combinations
-              combinations = Set.new(get_combinations(part))
+            # If all generated combinations exist in the input set
+            if combinations.subset?(geohashes)
+              # Add part to temporary output
+              final_geohashes.add(part)
+              # Add part to deleted geohash set
+              deletegh.add(part)
 
-              # If all generated combinations exist in the input set
-              if combinations.subset?(geohashes)
-                  # Add part to temporary output
-                  final_geohashes.add(part)
-                  # Add part to deleted geohash set
-                  deletegh.add(part)
-
-              # Else add the geohash to the temp out and deleted set
+            # Else add the geohash to the temp out and deleted set
+            else
+              deletegh.add(geohash)
+              
+              # Forced compression if geohash length is greater than max level after combination check failure
+              if geohash_length >= maxlevel
+                final_geohashes.add(geohash[:maxlevel])
               else
-                deletegh.add(geohash)
-
-                # Forced compression if geohash length is greater than max level after combination check failure
-                if geohash_length >= maxlevel
-                    final_geohashes.add(geohash[:maxlevel])
-                else
-                    final_geohashes.add(geohash)
-                end
+                final_geohashes.add(geohash)
               end
+            end
 
-              # Break if compressed output size same as the last iteration
-              if final_geohashes_size == final_geohashes.size
-                flag = false
-              end
+            # Break if compressed output size same as the last iteration
+            if final_geohashes_size == final_geohashes.size
+              flag = false
             end
           end
         end
+      end
 
-        final_geohashes_size = final_geohashes.size
+      final_geohashes_size = final_geohashes.size
+      geohashes.clear
 
-        geohashes.clear
-
-        # Temp output moved to the primary geohash set
-        geohashes = geohashes.union(final_geohashes)
+      # Temp output moved to the primary geohash set
+      geohashes = geohashes.union(final_geohashes)
     end
 
     return geohashes
